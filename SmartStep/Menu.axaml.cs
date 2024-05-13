767e75041512e2a339b1a352e638f4f9790b45a3
Zaphkie1ka <skyrim.skyrim.1001@gmail.com>
2024-05-13 21:26:39 +0700

    1

--- SmartStep/Menu.axaml.cs
+++ SmartStep/Menu.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MsBox.Avalonia;
 using SmartStep.Models;
 
 namespace SmartStep;
@@ -27,7 +28,7 @@ public partial class Menu : Window
     {
         InitializeComponent();
         //ShowBD();
-        GenerateColumnsForMonth();
+        
     }
 
     /*public void ShowBD()
@@ -43,7 +44,94 @@ public partial class Menu : Window
             Binding = new Binding("Models.Students.First_Name")
         };
         MainGrid.Columns.Add(studentColumn);
-        for (int day = 1; day <= DateTime.DaysInMonth(year, month); day++)
+
+        string[] months = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        comboBoxMonth.ItemsSource = months;
+
+        try
+        {
+            string selectedMonth = (string)comboBoxMonth.SelectedItem;
+            int monthNumber = Convert.ToInt32(selectedMonth);
+
+            int daysInMonth = DateTime.DaysInMonth(year, monthNumber);
+
+            for (int i = 1; i <= daysInMonth; i++)
+            {
+                DataGridTextColumn column = new DataGridTextColumn
+                {
+                    Header = i.ToString(),
+                    Binding = new Binding($"Day{i}")
+                };
+                MainGrid.Columns.Add(column);
+            }
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            // Обработка исключения
+            MessageBoxManager.GetMessageBoxStandard("Ошибка", "Пофиксите, разрабы!");
+        }
+
+        DataGridTextColumn dateColumn = new DataGridTextColumn
+        {
+            Header = "Дата проведения",
+            Binding = new Binding("Date")
+        };
+        MainGrid.Columns.Add(dateColumn);
+
+        DataGridTextColumn descriptionColumn = new DataGridTextColumn
+        {
+            Header = "Содержание занятия",
+            Binding = new Binding("Description")
+        };
+        MainGrid.Columns.Add(descriptionColumn);
+
+        DataGridTextColumn hoursColumn = new DataGridTextColumn
+        {
+            Header = "Часы занятия",
+            Binding = new Binding("Hours")
+        };
+        MainGrid.Columns.Add(hoursColumn);
+
+        DataGridTextColumn noteColumn = new DataGridTextColumn
+        {
+            Header = "Примечание",
+            Binding = new Binding("Note")
+        };
+        MainGrid.Columns.Add(noteColumn);
+    }
+    /*private void GenerateColumnsForMonth()
+    {
+        MainGrid.Columns.Clear();
+        DataGridTextColumn studentColumn = new DataGridTextColumn
+        {
+            Header = "ФИО ученика",
+            Binding = new Binding("Models.Students.First_Name")
+        };
+        MainGrid.Columns.Add(studentColumn);
+        try
+        {
+            string[] months = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+            comboBoxMonth.ItemsSource = months;
+            string selectedMonth = (string)comboBoxMonth.SelectedItem;
+            int monthNumber = Convert.ToInt32(selectedMonth);
+            
+            int daysInMonth = DateTime.DaysInMonth(year, monthNumber);
+
+            for (int i = 1; i <= daysInMonth; i++)
+            {
+                DataGridTextColumn column = new DataGridTextColumn
+                {
+                    Header = i.ToString(),
+                    Binding = new Binding($"Day{i}")
+                };
+                MainGrid.Columns.Add(column);
+            }
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            MessageBoxManager.GetMessageBoxStandard("Ошибка", "Пофиксите, разрабы!");
+        }
+        /*for (int day = 1; day <= DateTime.DaysInMonth(year, month); day++)
         {
             DataGridTextColumn column = new DataGridTextColumn
             {
@@ -52,7 +140,7 @@ public partial class Menu : Window
                 Width = new DataGridLength(50)
             };
             MainGrid.Columns.Add(column);
-        }
+        }#1#
         DataGridTextColumn dateColumn = new DataGridTextColumn
         {
             Header = "Дата проведения",
@@ -77,7 +165,7 @@ public partial class Menu : Window
             Binding = new Binding("Note")
         };
         MainGrid.Columns.Add(noteColumn);
-    }
+    }*/
     private void Button_Add_Student(object? sender, RoutedEventArgs e)
     {
         StudentWindow windowstud = new StudentWindow();
@@ -120,5 +208,15 @@ public partial class Menu : Window
     private void Button_Upd_Safety(object? sender, RoutedEventArgs e)
     {
         
+    }
+
+    private void Update_Table(object? sender, RoutedEventArgs e)
+    {
+        //GenerateColumnsForMonth();
+    }
+
+    private void ComboBoxMonth_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        GenerateColumnsForMonth();
     }
 }
--- SmartStep/obj/Debug/net8.0/SmartStep.AssemblyInfo.cs
+++ SmartStep/obj/Debug/net8.0/SmartStep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartStep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef0e16b97c7553815aae6eeb88c54ce5b3ec5c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b84516d76ca164908b0583c6b1949d739617ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartStep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartStep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
